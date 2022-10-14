5715eefa493fecd8c469cf2c1a6e190d38d4aeae
Wallace Júnior <wflj1997@hotmail.com>
2022-10-13 21:49:37 -0300

    random dog api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
-export const App: React.FC = () => (
-  <div className="flex min-h-screen items-center justify-center bg-zinc-900">
-    <h1 className="text-4xl font-bold text-zinc-100">Hello World</h1>
-  </div>
-);
+import { CircleNotch } from "phosphor-react";
+import { useEffect, useState } from "react";
+import { RandomDogAPI } from "./api/RandomDogAPI";
+
+export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState("");
+
+  async function fetchRandomDogImage() {
+    setIsLoading(true);
+    try {
+      const { url } = await RandomDogAPI.getRandomDog();
+      setImageUrl(url);
+    } catch (error) {
+      console.log(error);
+      alert("Ocorreu um erro ao buscar por outro cachorro.");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchRandomDogImage();
+  }, []);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-900">
+      <div className="flex flex-col gap-4">
+        <img
+          src={imageUrl}
+          alt=""
+          className="aspect-square w-64 rounded object-cover"
+        />
+
+        <button
+          onClick={fetchRandomDogImage}
+          className="flex cursor-pointer items-center justify-center rounded bg-indigo-600 px-4 py-3 text-zinc-100 transition-colors duration-300 hover:enabled:bg-indigo-700 disabled:cursor-not-allowed disabled:bg-zinc-700"
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <CircleNotch size={24} className="animate-spin" />
+          ) : (
+            "Carregar outro cachorro"
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
